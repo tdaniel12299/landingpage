ee329d7860a2316c9809fb27d4d2c3c88a740465
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2023-08-05 15:17:59 -0400

    overlay /menu transition

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,19 @@
 const btnMenu = document.querySelector('#btnMenu');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
 
 btnMenu.addEventListener('click',function(){
     console.log("open");
 
-    if(btnMenu.classList.contains('open')) {
-        btnMenu.classList.remove('open')
-    }else {
-        btnMenu.classList.add('open');
+    if(header.classList.contains('open')) { //close menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+
+    }else { //open menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
 })
