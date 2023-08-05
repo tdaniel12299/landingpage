a2bfaa1894b1a63051e9b55b079167d21148389a
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2023-08-05 13:23:29 -0400

    btn javascript added

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const btnMenu = document.querySelector('#btnMenu');
+
+btnMenu.addEventListener('click',function(){
+    console.log("open");
+
+    if(btnMenu.classList.contains('open')) {
+        btnMenu.classList.remove('open')
+    }else {
+        btnMenu.classList.add('open');
+    }
+})
