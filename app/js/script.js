d7d4402601876423908145324a531a67afb7a935
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2023-08-06 23:20:36 -0400

    mobile drop down menu complete

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,22 @@
 const btnMenu = document.querySelector('#btnMenu');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
-
+const body = document.querySelector('body');
 
 btnMenu.addEventListener('click',function(){
     console.log("open");
 
     if(header.classList.contains('open')) { //close menu
+        body.classList.remove('noscroll');
         header.classList.remove('open');
         overlay.classList.remove('fade-in');
         overlay.classList.add('fade-out');
 
     }else { //open menu
+        body.classList.add('noscroll');
         header.classList.add('open');
         overlay.classList.remove('fade-out');
         overlay.classList.add('fade-in');
+        
     }
 })
